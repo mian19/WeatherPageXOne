328b7e64fb61df1a6fb7938430f2d488816b3234
Anton Mishkevich <mian19@protonmail.com>
2022-11-23 01:58:32 +0300

    refactor: update VC and ThisDayTableView

--- WeatherPageXOne/WeatherPageXOne/Views/ThisDayTableView.swift
+++ WeatherPageXOne/WeatherPageXOne/Views/ThisDayTableView.swift
@@ -9,7 +9,6 @@ import UIKit
 
 class ThisDayTableView: UITableView {
     
-    private var reuseID = "reuseID"
     var textForFirstCell = ""
     var thisDayWeatherArray = [WeatherPerDay]()
     
@@ -17,15 +16,10 @@ class ThisDayTableView: UITableView {
         super.init(frame: frame, style: style)
         register(ThisDayFirstTableViewCell.self, forCellReuseIdentifier: ThisDayFirstTableViewCell.id)
         register(ThisDaySecondTableViewCell.self, forCellReuseIdentifier: ThisDaySecondTableViewCell.id)
-        translatesAutoresizingMaskIntoConstraints = false
-        backgroundColor = UIColor(red: 0.235, green: 0.306, blue: 0.396, alpha: 0.6)
-        layer.cornerRadius = 14
-        clipsToBounds = true
-        separatorColor = UIColor(red: 0.4314, green: 0.4706, blue: 0.5294, alpha: 1.0)
-        separatorInset = UIEdgeInsets(top: 0, left: 14.adjustSize(), bottom: 0, right: 14.adjustSize())
-        isScrollEnabled = false
         delegate = self
         dataSource = self
+        isScrollEnabled = false
+        setCustomStyle()
     }
     
     required init?(coder: NSCoder) {
--- WeatherPageXOne/WeatherPageXOne/Views/ViewController.swift
+++ WeatherPageXOne/WeatherPageXOne/Views/ViewController.swift
@@ -8,13 +8,95 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private var cityLabel: UILabel!
+    private var currentWeatherLabel: UILabel!
+    private var thisDayWeatherTable: ThisDayTableView!
+    private var forecastWeatherTable: ForecastTableView!
+    private var testData: JSONResponse!
+    private let presenter = Presenter()
+    weak private var viewOutputDelegate: ViewOutputDelegate?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        view.backgroundColor = .red
+        setViews()
+        setRelationsWithPresenter()
+        self.viewOutputDelegate?.getData()
+        displayData()
     }
+    
+    private func setRelationsWithPresenter() {
+        presenter.setViewInputDelegate(viewInputDelegate: self)
+        self.viewOutputDelegate = presenter
+    }
+    
+    private func setViews() {
+        view.addBackground()
+        setCityLabel()
+        setCurrentWeatherLabel()
+        setThisDayWeatherTable()
+        setForecastWeatherTable()
+    }
+    
+    private func setCityLabel() {
+        cityLabel = UILabel.customLabel(fontSize: 36.adjustSize())
+        view.addSubview(cityLabel)
+        
+        cityLabel.widthAnchor.constraint(equalToConstant: 130.adjustSize()).isActive = true
+        cityLabel.heightAnchor.constraint(equalToConstant: 36.adjustSize()).isActive = true
+        cityLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        cityLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 60.adjustSize()).isActive = true
+    }
+    
+    private func setCurrentWeatherLabel() {
+        currentWeatherLabel = UILabel.customLabel(fontSize: 20.adjustSize(), alpha: 0.6)
+        view.addSubview(currentWeatherLabel)
+        
+        currentWeatherLabel.widthAnchor.constraint(equalToConstant: 130.adjustSize()).isActive = true
+        currentWeatherLabel.heightAnchor.constraint(equalToConstant: 20.adjustSize()).isActive = true
+        currentWeatherLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        currentWeatherLabel.topAnchor.constraint(equalTo: cityLabel.bottomAnchor, constant: 7.adjustSize()).isActive = true
+    }
+    
+    private func setThisDayWeatherTable() {
+        thisDayWeatherTable = ThisDayTableView()
+        view.addSubview(thisDayWeatherTable)
+        
+        thisDayWeatherTable.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24.adjustSize()).isActive = true
+        thisDayWeatherTable.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24.adjustSize()).isActive = true
+        thisDayWeatherTable.heightAnchor.constraint(equalToConstant: 180.adjustSize()).isActive = true
+        thisDayWeatherTable.topAnchor.constraint(equalTo: currentWeatherLabel.bottomAnchor, constant: 57.adjustSize()).isActive = true
+    }
+    
+    private func setForecastWeatherTable() {
+        forecastWeatherTable = ForecastTableView()
+        view.addSubview(forecastWeatherTable)
+        
+        forecastWeatherTable.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24.adjustSize()).isActive = true
+        forecastWeatherTable.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24.adjustSize()).isActive = true
+        forecastWeatherTable.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -18.adjustSize()).isActive = true
+        forecastWeatherTable.topAnchor.constraint(equalTo: thisDayWeatherTable.bottomAnchor, constant: 9.adjustSize()).isActive = true
+    }
+    
+    //MARK: - set white status bar
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+}
 
-
+extension ViewController: ViewInputDelegate {
+    func displayData() {
+        cityLabel.text = testData.city
+        cityLabel.setCharacterSpacing(3)
+        currentWeatherLabel.text = testData.temperature
+        thisDayWeatherTable.textForFirstCell = testData.description
+        thisDayWeatherTable.thisDayWeatherArray = testData.weather_per_day
+        forecastWeatherTable.forecast = testData.forecast
+    }
+    
+    func setupData(testData: JSONResponse) {
+        self.testData = testData
+    }
 }
 
